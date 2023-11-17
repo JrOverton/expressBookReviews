10d31d9a77fb9aa2daf25586396edcb4c6ddb40d
Jessica <jessicarose.overton@gmail.com>
2023-11-17 12:28:09 -0500

    changes made from theia lab environment

--- final_project/router/auth_users.js
+++ final_project/router/auth_users.js
@@ -69,6 +69,23 @@ regd_users.put("/auth/review/:isbn", (req, res) => {
     }
   });
 
+  regd_users.delete("/auth/review/:isbn", (req,res) => { 
+    const isbn = req.params.isbn;
+    let filtered_book = books[isbn]
+    if (filtered_book) {
+        let review = req.query.review;
+        let reviewer = req.session.authorization['username'];
+        if(review) {
+            filtered_book['reviews'][reviewer] = review;
+            books[isbn] = filtered_book;
+        }
+        res.send(`The review for the book with ISBN  ${isbn} has been deleted.`);
+    }
+    else{
+        res.send("Unable to find this ISBN!");
+    }
+  });
+
 module.exports.authenticated = regd_users;
 module.exports.isValid = isValid;
 module.exports.users = users;
